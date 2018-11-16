3492a41b0836fe7b978eb17168f4402be6f68c77
JuniperPhoton <dengweichao@hotmail.com>
2018-11-16 22:32:44 +0800

    Use window insets to fit transparent navigation bar

--- app/src/main/kotlin/com/juniperphoton/myersplash/activity/BaseActivity.kt
+++ app/src/main/kotlin/com/juniperphoton/myersplash/activity/BaseActivity.kt
@@ -1,35 +1,40 @@
 package com.juniperphoton.myersplash.activity
 
 import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowInsets
 import android.view.WindowManager
 import com.juniperphoton.myersplash.R
-import com.juniperphoton.myersplash.extension.getNavigationBarSize
 import com.juniperphoton.myersplash.extension.getStatusBarHeight
 import com.juniperphoton.myersplash.extension.updateDimensions
+import com.juniperphoton.myersplash.utils.Pasteur
 
-open class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity : AppCompatActivity(), View.OnApplyWindowInsetsListener {
+    companion object {
+        private const val TAG = "BaseActivity"
+    }
+
     private var systemUiConfigured = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (Build.VERSION.SDK_INT >= 21) {
-            window.statusBarColor = Color.TRANSPARENT
-        }
+        window.statusBarColor = Color.TRANSPARENT
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
     }
 
     override fun onStart() {
         super.onStart()
+
+        val content = findViewById<View>(android.R.id.content)
+        content.setOnApplyWindowInsetsListener(this)
+
         if (!systemUiConfigured) {
             systemUiConfigured = true
-            onConfigNavigationBar(getNavigationBarSize().y)
             onConfigStatusBar()
         }
     }
@@ -39,5 +44,11 @@ open class BaseActivity : AppCompatActivity() {
                 ViewGroup.LayoutParams.MATCH_PARENT, getStatusBarHeight())
     }
 
-    open fun onConfigNavigationBar(navigationBarHeight: Int) = Unit
+    open fun onApplySystemInsets(top: Int, bottom: Int) = Unit
+
+    override fun onApplyWindowInsets(v: View, insets: WindowInsets): WindowInsets {
+        Pasteur.info(TAG, "height: ${insets.systemWindowInsetBottom}")
+        onApplySystemInsets(insets.systemWindowInsetTop, insets.systemWindowInsetBottom)
+        return insets.consumeSystemWindowInsets()
+    }
 }
