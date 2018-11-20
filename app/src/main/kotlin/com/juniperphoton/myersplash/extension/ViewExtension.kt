3ddd31d352dfd633d1e30f327dc041b8266145e6
JuniperPhoton <dengweichao@hotmail.com>
2018-11-20 19:57:35 +0800

    Use Coroutine in extractThemeColor

--- app/src/main/kotlin/com/juniperphoton/myersplash/extension/ViewExtension.kt
+++ app/src/main/kotlin/com/juniperphoton/myersplash/extension/ViewExtension.kt
@@ -16,6 +16,14 @@ fun View.getActivity(): Activity? {
     return null
 }
 
+fun View.updateVisibility(visible: Boolean) {
+    visibility = if (visible) {
+        View.VISIBLE
+    } else {
+        View.GONE
+    }
+}
+
 fun View.addDimensions(width: Int?, height: Int?) {
     val lp = layoutParams ?: ViewGroup.LayoutParams(0, 0)
     width?.let {
--- app/src/main/kotlin/com/juniperphoton/myersplash/utils/PaletteUtil.kt
+++ app/src/main/kotlin/com/juniperphoton/myersplash/utils/PaletteUtil.kt
@@ -4,10 +4,25 @@ import android.graphics.BitmapFactory
 import androidx.palette.graphics.Palette
 import com.juniperphoton.myersplash.model.UnsplashImage
 import io.reactivex.Maybe
+import kotlinx.coroutines.coroutineScope
 
+private const val IN_SAMPLE_SIZE = 4
+
+suspend fun UnsplashImage.extractThemeColor(): Int = coroutineScope {
+    val file = FileUtil.getCachedFile(listUrl!!) ?: kotlin.run {
+        return@coroutineScope Int.MIN_VALUE
+    }
+
+    val o = BitmapFactory.Options()
+    o.inSampleSize = IN_SAMPLE_SIZE
+
+    val bm = BitmapFactory.decodeFile(file.absolutePath, o)
+
+    return@coroutineScope Palette.from(bm).generate().darkVibrantSwatch?.rgb ?: Int.MIN_VALUE
+}
+
+@Deprecated("Use UnsplashImage.extractThemeColor() instead")
 object PaletteUtil {
-    private const val IN_SAMPLE_SIZE = 4
-
     fun extractThemeColorFromUnsplashImage(image: UnsplashImage?): Maybe<Int> {
         return Maybe.create { emitter ->
             image ?: kotlin.run {
