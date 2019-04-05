dbf9a93d1161fe2dd25a9ea7c81bee16fc47f3aa
dengweichao@hotmail.com <dengweichao@hotmail.com>
2019-04-05 23:48:50 +0800

    Use new domain

--- app/src/main/kotlin/com/juniperphoton/myersplash/cloudservice/Request.kt
+++ app/src/main/kotlin/com/juniperphoton/myersplash/cloudservice/Request.kt
@@ -1,15 +1,16 @@
 package com.juniperphoton.myersplash.cloudservice
 
 object Request {
+    const val MY_DOMAIN = "juniperphoton.dev"
     const val BASE_URL = "https://api.unsplash.com/"
     const val PHOTO_URL = "https://api.unsplash.com/photos?"
     const val FEATURED_PHOTO_URL = "https://api.unsplash.com/collections/featured?"
-    const val HIGHLIGHTS_PHOTO_URL = "http://juniperphoton.net/myersplash/thumbs"
+    const val HIGHLIGHTS_PHOTO_URL = "http://$MY_DOMAIN/myersplash/thumbs"
     const val RANDOM_PHOTOS_URL = "https://api.unsplash.com/photos/random?"
     const val SEARCH_URL = "https://api.unsplash.com/search/photos?"
 
-    const val AUTO_CHANGE_WALLPAPER = "https://juniperphoton.net/myersplash/wallpapers/"
-    const val AUTO_CHANGE_WALLPAPER_THUMB = "https://juniperphoton.net/myersplash/wallpapers/thumbs/"
+    const val AUTO_CHANGE_WALLPAPER = "https://$MY_DOMAIN/myersplash/wallpapers/"
+    const val AUTO_CHANGE_WALLPAPER_THUMB = "https://$MY_DOMAIN/myersplash/wallpapers/thumbs/"
 
     const val ME_HOME_PAGE = "https://unsplash.com/@juniperphoton"
 }
