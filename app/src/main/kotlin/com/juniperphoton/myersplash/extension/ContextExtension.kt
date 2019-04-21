8f3abc6dd6fa31946cbb13a9301dba9cd929bd9c
dengweichao@hotmail.com <dengweichao@hotmail.com>
2019-04-21 16:53:50 +0800

    Use new style for version text

--- app/src/main/kotlin/com/juniperphoton/myersplash/extension/ContextExtension.kt
+++ app/src/main/kotlin/com/juniperphoton/myersplash/extension/ContextExtension.kt
@@ -61,7 +61,7 @@ fun Context.getVersionName(): String? {
     return try {
         val manager = packageManager
         val info = manager.getPackageInfo(packageName, 0)
-        "Build ${info.versionName}"
+        "Version ${info.versionName}"
     } catch (e: Exception) {
         null
     }
