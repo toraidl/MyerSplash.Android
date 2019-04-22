ca9c6839a3cd5caccbcdab47d094773ce2d289b5
JuniperPhoton <dengweichao@hotmail.com>
2019-04-22 12:02:19 +0800

    Code cleanup

--- app/src/main/kotlin/com/juniperphoton/myersplash/utils/SimpleControllerListener.kt
+++ app/src/main/kotlin/com/juniperphoton/myersplash/utils/SimpleControllerListener.kt
@@ -6,32 +6,16 @@ import com.facebook.drawee.controller.ControllerListener
 import com.facebook.imagepipeline.image.ImageInfo
 
 open class SimpleControllerListener : ControllerListener<ImageInfo> {
-    override fun onIntermediateImageFailed(id: String?, throwable: Throwable?) {
-    }
-
-    override fun onRelease(id: String?) {
-    }
-
-    override fun onIntermediateImageSet(id: String?, imageInfo: ImageInfo?) {
-    }
-
-    override fun onSubmit(id: String?, callerContext: Any?) {
-    }
-
-    override fun onFailure(id: String?, throwable: Throwable?) {
-    }
-
-    override fun onFinalImageSet(id: String?, imageInfo: ImageInfo?, animatable: Animatable?) {
-    }
+    override fun onIntermediateImageFailed(id: String?, throwable: Throwable?) = Unit
+    override fun onRelease(id: String?) = Unit
+    override fun onIntermediateImageSet(id: String?, imageInfo: ImageInfo?) = Unit
+    override fun onSubmit(id: String?, callerContext: Any?) = Unit
+    override fun onFailure(id: String?, throwable: Throwable?) = Unit
+    override fun onFinalImageSet(id: String?, imageInfo: ImageInfo?, animatable: Animatable?) = Unit
 }
 
 open class SimpleOnSeekBarChangeListener : SeekBar.OnSeekBarChangeListener {
-    override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-    }
-
-    override fun onStartTrackingTouch(seekBar: SeekBar) {
-    }
-
-    override fun onStopTrackingTouch(seekBar: SeekBar) {
-    }
+    override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) = Unit
+    override fun onStartTrackingTouch(seekBar: SeekBar) = Unit
+    override fun onStopTrackingTouch(seekBar: SeekBar) = Unit
 }
